47b0de9059de76101ae3adebf3f6352906419924
Ashton Drew <ashton.drew@gmail.com>
2023-11-01 18:51:15 +0000

    function get_completions works

--- function_get_completions.R
+++ function_get_completions.R
@@ -6,23 +6,23 @@ library(openai)
 
 #######################
 # Python version
-def get_completion(prompt, model="gpt-3.5-turbo"):
-  messages = [{"role": "user", "content": prompt}]
-  response = openai.ChatCompletion.create(
-    model=model,
-    messages=messages,
-    temperature=0, # this is the degree of randomness of the model's output
-  )
-  return response.choices[0].message["content"]
+# def get_completion(prompt, model="gpt-3.5-turbo"):
+#   messages = [{"role": "user", "content": prompt}]
+#   response = openai.ChatCompletion.create(
+#     model=model,
+#     messages=messages,
+#     temperature=0, # this is the degree of randomness of the model's output
+#   )
+#   return response.choices[0].message["content"]
 ########################
   
 get_completions <- function(prompt, model = "gpt-3.5-turbo") {
     messages <- list(list("role" = "user", "content" = prompt))
     response <- openai::create_chat_completion(model = model, messages = messages, temperature = 0)
-    return(response$choices)
+    return(response$choices$message.content)
   }
   
 model <- "gpt-3.5-turbo"
 prompt <- "Generate a list of three made-up book titles along with their authors and genres. Provide them in JSON format with the following keys:book_id, title, author, genre."
 
-get_completions(model, prompt)
+answer <- get_completions(prompt = prompt, model = model)
